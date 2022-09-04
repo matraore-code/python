b1187551be44278a7e07654c1d46f5156a680219
matraore-code <matraore-code@protonmail.com>
2022-09-04 18:26:27 +0000

    Day01 of 100 days of python

---
+++ day01/main.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("Welcome to the brand name generator.\n")
+#2. Ask the user for the city that they grew up in.
+birthPlace = input("What's name of the city you grew up in ?\n")
+#3. Ask the user for the name of a pet.
+petName = input("What's your pet's name ?\n")
+#4. Combine the name of their city and pet and show them their band name.
+#brandName = birthPlace + petName
+print("The name of your brand you could be " + birthPlace + " " + petName)
+
